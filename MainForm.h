196b25556e48661f75c8aa9da83f177d91be8e86
Mohan Babu Pothina <154684033+mohanpothina1@users.noreply.github.com>
2024-06-05 14:08:11 +0530

    Add files via upload
    Declare MainForm class with navigation methods

---
+++ MainForm.h
@@ -0,0 +1,27 @@
+//---------------------------------------------------------------------------
+
+#ifndef MainFormH
+#define MainFormH
+//---------------------------------------------------------------------------
+#include <System.Classes.hpp>
+#include <FMX.Controls.hpp>
+#include <FMX.Forms.hpp>
+#include <FMX.Controls.Presentation.hpp>
+#include <FMX.StdCtrls.hpp>
+#include <FMX.Types.hpp>
+//---------------------------------------------------------------------------
+class TMyMainForm : public TForm
+{
+__published:	// IDE-managed Components
+	TButton *loginNavigationButton;
+	TButton *registrationNavigationButton;
+	void __fastcall loginNavigationButtonClick(TObject *Sender);
+	void __fastcall registrationNavigationButtonClick(TObject *Sender);
+private:	// User declarations
+public:		// User declarations
+	__fastcall TMyMainForm(TComponent* Owner);
+};
+//---------------------------------------------------------------------------
+extern PACKAGE TMyMainForm *MyMainForm;
+//---------------------------------------------------------------------------
+#endif
